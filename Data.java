36337aef4c448a0ccc84bd4914e0065429d8b878
Rohit Mali <144763468+rohitmaliarya@users.noreply.github.com>
2024-07-19 09:48:10 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,30 @@
+public class Data {
+    public static void main(String[] args) {
+      
+        int expr1 = 23 - 6;
+        int expr2 = 45 * 2;
+        int expr3 = 56 / 2;
+        int expr4 = 45 + 78 + 23 + 12;
+        int expr5 = 56 * 34 + 12 / 2 - 13;
+        double expr6 = 1.2 * 6.7 - 4.5 / 2;
+        double expr7 = 1.6 * (12 - 2) / 2 + 7.8 - 1.5;
+        double expr8 = 4.5 * 1.2 - (82 / 2) - 5.6 + 1.234;
+        double expr9 = 8 * 1 + (4 - 2) / 1 + 6;
+        double expr10 = 3.4 + 1.2 * (3.7 - 1.2) / 2 + 6.8;
+        double expr11 = 8.9 * 1.67 - (6.21 - 5.6) + 7.8 - 34 + 157;
+
+        System.out.println("23 - 6 = " + expr1 + " (int)");
+        System.out.println("45 * 2 = " + expr2 + " (int)");
+        System.out.println("56 / 2 = " + expr3 + " (int)");
+        System.out.println("45 + 78 + 23 + 12 = " + expr4 + " (int)");
+        System.out.println("56 * 34 + 12 / 2 - 13 = " + expr5 + " (int)");
+        System.out.println("1.2 * 6.7 - 4.5 / 2 = " + expr6 + " (double)");
+        System.out.println("1.6 * (12 - 2) / 2 + 7.8 - 1.5 = " + expr7 + " (double)");
+        System.out.println("4.5 * 1.2 - (82 / 2) - 5.6 + 1.234 = " + expr8 + " (double)");
+        System.out.println("8 * 1 + (4 - 2) / 1 + 6 = " + expr9 + " (double)");
+        System.out.println("3.4 + 1.2 * (3.7 - 1.2) / 2 + 6.8 = " + expr10 + " (double)");
+        System.out.println("8.9 * 1.67 - (6.21 - 5.6) + 7.8 - 34 + 157 = " + expr11 + " (double)");   
+        System.out.println("8.9 * 1.67 - (6.21 - 5.6) + 7.8 - 34 + 157 = " + expr11 + " (double)");
+       
+    }
+}
---
+++ User.java
@@ -0,0 +1,14 @@
+public class User{
+    public static void main(String[] args) {
+        //2.35
+        float a=2.35f;
+        System.out.println(a);
+        //1242
+        int b=1242;
+        System.out.println(b);
+        float c=4.56f;
+        System.out.println(c);
+        
+
+    }
+}
